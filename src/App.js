a209c5082db574bb313fc5a232789fc7f3c31424
Windows 10 <hungpham307@gmail.com>
2022-05-03 21:59:48 +0700

    fetch first 10 images

--- src/App.js
+++ src/App.js
@@ -9,9 +9,65 @@ const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState([])
+  const [query, setQuery] = useState('')
+
+  const fetchImages = async () => {
+    setLoading(true)
+    let url = `${mainUrl}${clientID}`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setPhotos(data);
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchImages()
+  }, [])
+
+  const handleChange = e => {
+    setQuery(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
 
   return (
-    <h1>Hello </h1>
+    <main>
+      <section className="search">
+        <form action="" className="search-form">
+          <input type="text"
+            className='form-input'
+            placeholder='search image'
+            value={query}
+            onChange={handleChange}
+          />
+          <button type='submit'
+            className="submit-btn"
+            onClick={handleSubmit}
+          >
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((photo, index) => {
+            return <Photo key={index} {...photo} />
+          })}
+        </div>
+
+        {loading && <h2 className='loading'>Loading...</h2>}
+      </section>
+    </main>
   );
 }
 
